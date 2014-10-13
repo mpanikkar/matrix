1fbfb3a79963d5f5ba2a26f78a022e0da0b8f9e6
AlexNeumann <alex.neumann92@gmail.com>
2014-10-12 17:53:49 -0700

    Update matrix_dimensional_SQL.sql

--- matrix_dimensional_SQL.sql
+++ matrix_dimensional_SQL.sql
@@ -11,32 +11,32 @@ DROP TABLE IF EXISTS customer_dim CASCADE;
 
 -- Store Dimension
 CREATE TABLE store_dim (
-	store_key				serial	NOT NULL	PRIMARY KEY,
-	store_number			smallint,
-	store_street			char(30),
+	store_key				serial		NOT NULL	PRIMARY KEY,
+	store_number				smallint,
+	store_street				char(30),
 	store_city				char(30),
 	store_state				char(2),
-	store_zipcode			char(6),
-	store_region			char(15),
-	store_representative	char(30)
+	store_zipcode				char(6),
+	store_region				char(15),
+	store_representative			char(30)
 );
 
 -- Customer Dimension
 CREATE TABLE cust_dim (
-	cust_key			serial		NOT NULL		PRIMARY KEY,
-	cust_firstname		char(30),
-	cust_lastname		char(30),
-	cust_dateofbirth		date,
-	cust_login			char(15),
-	cust_password		char(15),
-	cust_joindate			date
+	cust_key				serial		NOT NULL	PRIMARY KEY,
+	cust_firstname				char(30),
+	cust_lastname				char(30),
+	cust_dateofbirth			date,
+	cust_login				char(15),
+	cust_password				char(15),
+	cust_joindate				date
 );
 
 -- Date Dimension 
 CREATE TABLE date_dim (
-    date_key                       		serial      NOT NULL    PRIMARY KEY,
-    date                            	date,
-    day_number_in_week              	smallint,   -- sunday = 0, saturday = 6
+    date_key                       		serial  	NOT NULL	PRIMARY KEY,
+    date                            		date,
+    day_number_in_week              		smallint,   -- sunday = 0, saturday = 6
     day_number_in_calendar_month  		smallint,
     day_number_in_calendar_year    		smallint,
     calendar_week_number_in_year  		smallint,
@@ -46,35 +46,35 @@ CREATE TABLE date_dim (
     day_of_week                   		char(9),
     calendar_month_name      			char(9),
     full_date_description       		char(20),
-    calendar_year_month             	char(7),
+    calendar_year_month             		char(7),
     calendar_year_quarter          		char(6)
 );
 
 -- Line Dimension
 CREATE TABLE line_dim (
-	line_key			serial		NOT NULL		PRIMARY KEY,
-	line_number			bigint,
-line_description		char(30),
-line_price				int,
-line_datalimit			int,
-line_talklimit			int,
-line_textlimit			int
-);
+	line_key				serial		NOT NULL	PRIMARY KEY,
+	line_number				bigint,
+line_description				char(30),
+line_price					int,
+line_datalimit					int,
+line_talklimit					int,
+line_textlimit					int
+);	
 
 -- Create the Call Log Fact table
 -- Grain is one row per call in a call log
 CREATE TABLE call_fact (
 	-- foreign keys
-	date_key			integer		references date_dim(date_key),
-	store_key			integer		references store_dim(store_key),
-	cust_key			integer		references cust_dim(cust_key),
-	line_key			integer		references line_dim(line_key),
-	call_id				integer,	-- degenerate dimension
+	date_key				integer		references date_dim(date_key),
+	store_key				integer		references store_dim(store_key),
+	cust_key				integer		references cust_dim(cust_key),
+	line_key				integer		references line_dim(line_key),
+	call_id					integer,	-- degenerate dimension
 	-- facts
 	call_duration				numeric,
-	call_perminutecharge		money,
+	call_perminutecharge			money,
 	call_totalcharge			money,
-	call_totalcharge_tax		money,
+	call_totalcharge_tax			money,
 -- primary key
 PRIMARY KEY (call_id, line_key)
 );
